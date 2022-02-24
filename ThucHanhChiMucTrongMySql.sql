579c426357b4222191fbe9fafc24da27ff09854c
HoThanhLich <thanhlichpvmtc@gmail.com>
2022-02-24 10:39:46 +0700

    them file

---
+++ ThucHanhChiMucTrongMySql.sql
@@ -0,0 +1,6 @@
+select * from customers where customerName = 'Land of Toys Inc.';
+explain select * from customers where customerName = 'Land of Toys Inc.';
+alter table customers add index idx_customerName(customerName);
+alter table customers add index idx_full_name(contactFirstName, contactLastName);
+explain select * from customers where contactFirstName = 'Jean' or contactFirstName = 'King';;
+alter table customers drop index idx_full_name;
